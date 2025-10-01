7f781dd196291733d32c5d8d59dcf4fcb03d5118
knownasrayy <105399909+knownasrayy@users.noreply.github.com>
2025-10-01 23:05:59 +0700

    Update Soal_5.sh

--- Script/Soal_5.sh
+++ Script/Soal_5.sh
@@ -1,1 +1,10 @@
+Di console Eru
+nano /root/.bashrc
+apt update && apt install -y iptables 
+# Untuk Iptablesdi network config GNS3 dengan perintah:
+# up iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE -s 192.236.0.0/16
+
+Di semua console
+nano /root/.bashrc
+echo "nameserver 192.168.122.1" > /etc/resolv.conf
 
