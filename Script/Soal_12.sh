d40bc02138fe7ccaa60c59e99bc35a8f9badf8de
knownasrayy <105399909+knownasrayy@users.noreply.github.com>
2025-10-01 22:06:19 +0700

    Create Soal_12.sh

---
+++ Script/Soal_12.sh
@@ -0,0 +1,28 @@
+Di Eru
+
+apt update && apt install -y netcat-openbsd
+
+MELKOR=10.79.1.2
+nc -zv $MELKOR 21
+nc -zv $MELKOR 80
+nc -zv $MELKOR 666
+
+Di Melkor
+
+apt update && apt install -y vsftpd
+service vsftpd start
+
+ss -tlnp | grep :21
+
+apt install -y apache2
+service apache2 start
+
+ss -tlnp | grep :80
+
+Sebelum
+
+![Alt text](Assets/Soal_12/Screenshot_94.png)
+
+Sesudah
+
+![Alt text](Assets/Soal_12/Screenshot_95.png)
